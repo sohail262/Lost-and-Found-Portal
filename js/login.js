89c82df23c2e67accc035a678c4c64d607fa8f71
sohail262 <shaiksabira90@gmail.com>
2024-04-01 23:34:11 +0530

    added sweetalert's toast

--- js/login.js
+++ js/login.js
@@ -1,13 +1,12 @@
-// Firebase Configuration
 const firebaseConfig = {
   databaseURL: "https://lostandfound-e3d1d-default-rtdb.firebaseio.com/",
-apiKey: "AIzaSyDxDcnRl1NsWlKvXiDKFT_m5fgewHGv1H0",
-authDomain: "lostandfound-e3d1d.firebaseapp.com",
-projectId: "lostandfound-e3d1d",
-storageBucket: "lostandfound-e3d1d.appspot.com",
-messagingSenderId: "798654626600",
-appId: "1:798654626600:web:fb253dafc2aa9e47096e1d",
-measurementId: "G-H51RQ9Q3R1"
+  apiKey: "AIzaSyDxDcnRl1NsWlKvXiDKFT_m5fgewHGv1H0",
+  authDomain: "lostandfound-e3d1d.firebaseapp.com",
+  projectId: "lostandfound-e3d1d",
+  storageBucket: "lostandfound-e3d1d.appspot.com",
+  messagingSenderId: "798654626600",
+  appId: "1:798654626600:web:fb253dafc2aa9e47096e1d",
+  measurementId: "G-H51RQ9Q3R1"
 };
 
 // Initialize Firebase
@@ -23,20 +22,43 @@ loginForm.addEventListener('submit', (e) => {
 
   const email = loginForm['email'].value;
   const password = loginForm['password'].value;
+  
+const Toast = Swal.mixin({
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.onmouseenter = Swal.stopTimer;
+    toast.onmouseleave = Swal.resumeTimer;
+  }
+});
 
   auth.signInWithEmailAndPassword(email, password)
     .then((userCredential) => {
       // Signed in
       const user = userCredential.user;
       console.log('User logged in:', user);
-      window.location.href = 'main.html'; // Redirect to your portal page
+      setTimeout(() => {
+        window.location.href = 'main.html'; // Redirect to your portal page
+      }, 1500); // Redirect to your portal page
+
+      // Display Toast notification for successful login
+      Toast.fire({
+        icon: "success",
+        title: "Signed in successfully"
+      });
     })
     .catch((error) => {
-      const errorCode = error.code;
       const errorMessage = error.message;
-      console.error('Login error:', errorMessage);
-      // Display error message to the user
-      alert(errorMessage);
+      console.error('Login error');
+
+      // Display Toast notification for login error
+      Toast.fire({
+        icon: "error",
+        title: "Login Error, Please check your mail and password"
+      });
     });
 });
 
@@ -44,19 +66,19 @@ loginForm.addEventListener('submit', (e) => {
 const forgotPasswordLink = document.getElementById('forgot-password');
 
 forgotPasswordLink.addEventListener('click', (e) => {
-e.preventDefault();
+  e.preventDefault();
 
-const email = prompt('Enter your email to reset password:');
-if (email) {
-  auth.sendPasswordResetEmail(email)
-    .then(() => {
-      alert('Password reset email sent. Please check your inbox.');
-    })
-    .catch((error) => {
-      const errorMessage = error.message;
-      console.error('Forgot password error:', errorMessage);
-      // Display error message to the user
-      alert(errorMessage);
-    });
-}
+  const email = prompt('Enter your email to reset password:');
+  if (email) {
+    auth.sendPasswordResetEmail(email)
+      .then(() => {
+        alert('Password reset email sent. Please check your inbox.');
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        console.error('Forgot password error:', errorMessage);
+        // Display error message to the user
+        alert(errorMessage);
+      });
+  }
 });
